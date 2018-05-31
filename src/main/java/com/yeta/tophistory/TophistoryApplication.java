e2a0bb389f886dadce47a6ed931925b73463fa28
Yevgen Taran <yevgenii.taran@gmail.com>
2018-05-31 22:54:21 +0300

    'init'

---
+++ src/main/java/com/yeta/tophistory/TophistoryApplication.java
@@ -0,0 +1,12 @@
+package com.yeta.tophistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TophistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TophistoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yeta/tophistory/TophistoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yeta.tophistory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TophistoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
