547e6e6b0991b3d25ce8eb17d8fb237d7269f43d
Yevgen Taran <yevgenii.taran@gmail.com>
2018-09-09 18:02:00 +0300

    switch angular to angularjs

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -21,18 +21,14 @@ module.exports = function(grunt) {
                 }
             }
         },
-        qunit: {
-            files: ['test/**/*.html']
-        }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('test', ['qunit']);
 
-    grunt.registerTask('default', ['qunit', 'concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
