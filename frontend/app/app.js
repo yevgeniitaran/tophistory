db68ae6dc463866e2217202ee44e02d563d365b6
Yevgen Taran <yevgenii.taran@gmail.com>
2018-09-12 21:44:10 +0300

    angularjs build

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,14 +1,15 @@
-'use strict';
+angular.module('healthyGulpAngularApp', ['ui.router'])
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+
+            .state('home', {
+                url: '/',
+                templateUrl: 'components/home.html'
+            });
+
+    }]);
